da827ce5717495be0ee1dfa58d04c145a1c79ec0
Marius Reimer <reime005@gmail.com>
2021-02-03 23:51:40 +0000

    pokedex css

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,25 +1,79 @@
 <script lang="ts">
-  import Data from "./Data.svelte";
-  // import Pokedex from "./Pokedex.svelte";
+  import Pokedex from "./Pokedex.svelte";
 
   import { initClient } from "@urql/svelte";
+  import { operationStore, query } from "@urql/svelte";
+  import Data from "./Data.svelte";
 
   initClient({
-    url: "https://graphql-pokeapi.herokuapp.com/graphql",
+    // url: "https://graphql-pokeapi.herokuapp.com/graphql",
+    url: "http://localhost:8080/graphql",
   });
+
+  const qres = operationStore(
+    `
+    {
+      apiVersion
+    }
+    `
+  );
+  query(qres);
 </script>
 
 <main>
-  <Data />
-  <!-- <Pokedex /> -->
+  {#if $qres.fetching}
+    <p>Loading...</p>
+  {:else if $qres.error}
+    <p>Oh no... {$qres.error.message}</p>
+  {:else}
+    <div class="container">
+      <div class="fullWrap">
+        <Pokedex name="pikachu" />
+      </div>
+
+      <a
+        aria-label="Credits"
+        class="version"
+        href="https://codepen.io/Bidji/pen/MYdPwo">Credits</a
+      >
+      <a
+        aria-label="Source Code on Github"
+        class="version"
+        href="https://github.com/reime005/graphql_pokeapi_rust"
+        >Code on Github (v{qres.data.apiVersion})</a
+      >
+    </div>
+  {/if}
 </main>
 
 <style>
-
   main {
     height: 100vh;
     max-height: 100vh;
     overflow: hidden;
+  }
+
+  .container {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 100%;
+    padding: 2vw;
+  }
+
+  .fullWrap {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .version {
+    font-size: 0.8rem;
+    color: var(--grey);
+    align-self: flex-end;
   }
 
   h1 {
